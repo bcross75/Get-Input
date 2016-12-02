55f0df9d9fd7686c4aa9300d7b21615a66cf0ae0
bcross75 <bcross75@yahoo.com>
2016-12-01 22:03:59 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0} the current time is {1}", username, System.DateTime.Now.TimeOfDay);
+
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+
+        }
+    }
+}
